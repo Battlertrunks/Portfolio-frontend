3626041fc93ca61909a31f5ea3c140c1e361a210
Gavin <Gavinszczesniak@gmail.com>
2022-06-10 19:05:32 -0400

    Experimenting with designs with the contact page, I believe that it is almost done. Changed the header to have a background color and updated the about page.

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -10,23 +10,27 @@ const AboutMe = () => {
 
   return (
     <div className="AboutMe">
-      <div data-aos="fade-down" className="circle"></div>
-      <h4 className="word-background">Me.</h4>
-      <h3 data-aos="fade-up">About Me.</h3>
-      <img
-        data-aos="fade-up"
-        className="phone-img"
-        src="https://firebasestorage.googleapis.com/v0/b/gavinszczesniakportfolio.appspot.com/o/About-Me-Iphone.png?alt=media&token=cf581b03-de12-4278-8a29-790478082168"
-        alt="Mobile phone application design introducing Gavin Szczesniak."
-      />
-      <p data-aos="fade-up">
-        My name is Gavin Szczesniak and I am a MERN Stack Developer. As a child
-        I always had a fascination of coding. At first, I used code to create
-        video games using Unity Game Engine. During my high school days I also
-        found a passion for design. Not knowing at the time, this would lead me
-        to have a love for web developement. Piecing code and design together
-        was the passion I was looking for.
-      </p>
+      <div>
+        <div data-aos="fade-down" className="circle"></div>
+        <h4 className="word-background">Me.</h4>
+        <h3 data-aos="fade-up">About Me.</h3>
+        <div className="order-phone-description">
+          <img
+            data-aos="fade-up"
+            className="phone-img"
+            src="https://firebasestorage.googleapis.com/v0/b/gavinszczesniakportfolio.appspot.com/o/About-Me-Iphone.png?alt=media&token=cf581b03-de12-4278-8a29-790478082168"
+            alt="Mobile phone application design introducing Gavin Szczesniak."
+          />
+          <p data-aos="fade-up">
+            My name is Gavin Szczesniak and I am a MERN Stack Developer. As a
+            child I always had a fascination of coding. At first, I used code to
+            create video games using Unity Game Engine. During my high school
+            days I also found a passion for design. Not knowing at the time,
+            this would lead me to have a love for web developement. Piecing code
+            and design together was the passion I was looking for.
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/ContactRoute.tsx
+++ src/components/ContactRoute.tsx
@@ -3,33 +3,43 @@ import "./ContactRoute.css";
 const ContactRoute = () => {
   return (
     <div className="ContactRoute">
-      <h2>Pass Me a Message.</h2>
-      <form>
-        <label htmlFor="name">Name *</label>
+      <div className="contact-container">
         <div>
-          <input
-            type="text"
-            name="first-name"
-            id="first-name"
-            placeholder="First name..."
-          />
-          <input
-            type="text"
-            name="last-name"
-            id="last-name"
-            placeholder="Last name...."
+          <p className="opening-text">Let's connect</p>
+          <h2>Pass Me a Message.</h2>
+          <img
+            className="mail-icon"
+            src="https://firebasestorage.googleapis.com/v0/b/gavinszczesniakportfolio.appspot.com/o/send-mail-orange.png?alt=media&token=2c7b5832-eac2-4bd9-be90-8c08232641e0"
+            alt="paper plane message."
           />
         </div>
-        <label htmlFor="user-email">Email *</label>
-        <div>
-          <input type="email" name="user-email" id="user-email" />
-        </div>
-        <label htmlFor="message">Message *</label>
-        <div>
-          <textarea name="message" id="message"></textarea>
-        </div>
-        <button>Submit</button>
-      </form>
+        <form>
+          <label htmlFor="name">Name *</label>
+          <div className="name-input-container">
+            <input
+              type="text"
+              name="first-name"
+              id="first-name"
+              placeholder="First name..."
+            />
+            <input
+              type="text"
+              name="last-name"
+              id="last-name"
+              placeholder="Last name...."
+            />
+          </div>
+          <label htmlFor="user-email">Email *</label>
+          <div>
+            <input type="email" name="user-email" id="user-email" />
+          </div>
+          <label htmlFor="message">Message *</label>
+          <div>
+            <textarea name="message" id="message"></textarea>
+          </div>
+          <button>Submit</button>
+        </form>
+      </div>
     </div>
   );
 };
