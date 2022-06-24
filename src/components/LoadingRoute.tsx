2cca1b5e30c75c8caa6833604e52a2b0d04cd349
Gavin <Gavinszczesniak@gmail.com>
2022-06-24 12:39:42 -0400

    Added animation on the loading screen when viewing the projects page.

--- src/components/LoadingRoute.tsx
+++ src/components/LoadingRoute.tsx
@@ -4,7 +4,11 @@ const LoadingRoute = () => {
   // Loading page while retrieving project data.
   return (
     <div className="LoadingRoute">
-      <h3>Loading Page...</h3>
+      <div className="circle c1"></div>
+      <div className="circle c2"></div>
+      <div className="circle c3"></div>
+      <div className="circle c4"></div>
+      <div className="circle c5"></div>
     </div>
   );
 };
